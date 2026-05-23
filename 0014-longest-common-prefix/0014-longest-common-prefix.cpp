36328e48085ec4361b7302a31bcd6f86f5189141
MUKUL PRASAD <mukulprasad958@gmail.com>
2026-05-23 09:30:53 +0530

    Time: 0 ms (100%), Space: 12.1 MB (14.5%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        int n=strs.size();
+        sort(strs.begin(),strs.end());
+        string first=strs[0];
+        string last=strs[n-1];
+        string result="";
+        for(int i=0;i<first.size();i++)
+        {
+            if(first[i]!=last[i]) break;
+
+            result+=first[i];
+        }
+        return result;
+
+    }
+};
